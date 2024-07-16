db32305697fe9a5429542c2076fa510187b4b481
James Tu <jamestvvo@gmail.com>
2024-07-15 20:56:46 -0400

    add move audio

--- client/JChess/src/App.tsx
+++ client/JChess/src/App.tsx
@@ -1,9 +1,15 @@
 import { useState } from "react";
+import { useSound } from "use-sound"
 import {Board} from "./components/chessboard/board";
 import { MoveDisplay } from './components/moveDisplay/moveDisplay';
 import { Chess, Square} from 'chess.js';
 import './App.css';
 
+import moveSound from "./assets/audio/move-self.mp3";
+import captureSound from "./assets/audio/capture.mp3";
+import castleSound from "./assets/audio/castle.mp3";
+import promoteSound from "./assets/audio/promote.mp3";
+import checkSound from "./assets/audio/move-check.mp3";
 
 const App = () => {
   const [chess] = useState(new Chess());
@@ -12,26 +18,59 @@ const App = () => {
   const [position, setPosition] = useState('start'); // fen string representation of position
   const [gameHistory, setGameHistory] = useState(chess.history({verbose: true}))
   const [moveCount, setMoveCount] = useState(1)
+  // sound effects
+  const [playMoveSound] = useSound(moveSound)
+  const [playCaptureSound] = useSound(captureSound)
+  const [playCastleSound] = useSound(castleSound)
+  const [playPromoteSound] = useSound(promoteSound)
+  const [playCheckSound] = useSound(checkSound)
 
-  const onDrop = (sourceSquare, targetSquare) => {
+  const onDrop = (sourceSquare: string, targetSquare: string) => {
 
     try {
-      chess.move({
+      const move = chess.move({
         from: sourceSquare,
         to: targetSquare,
         promotion: 'q' // always promote to queen for simplicity
       });
+      setGameHistory(chess.history({verbose: true}))
+      setPosition(chess.fen())
+      setDisplayPosition(chess.fen())
+      setMoveCount(chess.history().length)
+      playMoveAudio(move)
     } catch (error) {
+        console.log(error)
         console.log("Illegal Move!")
     }
-
-    setGameHistory(chess.history({verbose: true}))
-    setPosition(chess.fen());
-    setDisplayPosition(chess.fen())
-    setMoveCount(chess.history().length)
   };
 
-  const setBoardToPos = (pos) => {
+  const playMoveAudio = (move) => {
+    if (chess.inCheck()) {
+      playCheckSound()
+      return;
+    }
+    const flag = move.flags
+    console.log(flag)
+    console.log("here")
+    switch (flag) {
+      case "c":
+      case "pc":
+      case "e":
+        playCaptureSound()
+        break
+      case "p":
+        playPromoteSound()
+        break
+      case "k":
+      case "q":
+        playCastleSound()
+        break;
+      default:
+        playMoveSound()
+    }
+  }
+
+  const setBoardToPos = (pos: string) => {
     setDisplayPosition(pos)
     setDraggable(pos === position)
   }
